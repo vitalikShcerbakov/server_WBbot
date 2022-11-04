3cb7d7517867f0c5724f468923af704a4b4c0456
vitalik <sandwich.htc@gmail.com>
2022-11-05 00:54:30 +0300

    Added users datebase

--- main.py
+++ main.py
@@ -14,54 +14,19 @@ from settings import TG_TOKEN
 bot = telebot.TeleBot(TG_TOKEN)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Просмотреть список артикулов")
-    btn2 = types.KeyboardButton("Запустить проверку")
-    btn3 = types.KeyboardButton("Просмотр последней проверки")
-    markup.add(btn1, btn2, btn3)
-    bot.send_message(
-        message.chat.id, text="Привет, {0.first_name}! Я bot версии 0.9 =))"
-        .format(message.from_user), reply_markup=markup)
+def get_vendor_code():
 
-
-@bot.message_handler(content_types=['text'])
-def func(message):
-    global chat_id
-    chat_id = message.chat.id
-    if (message.text == "Запустить проверку"):
-        bot.send_message(
-            message.chat.id, 'Проверка займет некоторое время, подожди....')
-        start_check_vc()
-
-    elif message.text == "Просмотреть список артикулов":
-        answer = show_result_check()
-        for line in answer:
-            bot.send_message(message.chat.id, str(line))
-
-    elif message.text == 'Просмотр последней проверки':
-        answer = read_from_datebase()
-        for line in answer:
-            bot.send_message(message.chat.id, str(line))
-    else:
-        if download_article_list(message.text):
-            bot.send_message(message.chat.id, 'Cписок загружен')
-        else:
-            bot.send_message(
-                message.chat.id, 'Разделитель должен быть пробел или ","')
-            bot.send_message(message.chat.id, 'должы быть только цифры')
-
-
-def get_vendor_code(list_vc):
+    result = read_from_datebase()
+    list_vc:list[int] =  [int(value[0]) for value in result if value is not None]
     list_vendor_code = []
+
     options_chrome = webdriver.ChromeOptions()
     options_chrome.add_argument('--headless')
 
     with webdriver.Chrome(
         options=options_chrome,
          executable_path='/usr/local/bin/chromedriver') as browser:
-        for vc in list_vc:
+        for i, vc in enumerate(list_vc):
             line = []
             url = f'https://www.wildberries.ru/catalog/{vc}/detail.aspx?targetUrl=BP'
             try:
@@ -69,9 +34,10 @@ def get_vendor_code(list_vc):
                 time.sleep(3)
                 lst_value = browser.find_element(
                     By.CLASS_NAME, 'delivery__store')
-                answer = f'{url} {lst_value.text}'
+                #answer = f'{url} {lst_value.text}'
                 line.append(vc)
-                line.append(answer)
+                line.append(url)
+                line.append(lst_value.text)
                 if lst_value.text.find('со склада продавца') != -1:
                     line.append(True)
                 else:
@@ -80,10 +46,22 @@ def get_vendor_code(list_vc):
                 line.append(vc)
                 line.append(url)
                 line.append('Error')
+                line.append('Error')
+            finally:
+                progress_bar = i / len(list_vc) * 100
+                print(f'Progress: {round(progress_bar, 1)} % {int(progress_bar) * "#"} {i}/{len(list_vc)}')
 
             list_vendor_code.append(line)
-    return list_vendor_code
+    date_now = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+    print(f'Время последнией проверки: {date_now}')
+    write_to_database(list_vendor_code)
 
+
+def notification_on_off(chad_id, flag):
+    with open('users_datebase.csv', 'r') as csv_file:
+        file = csv.reader(csv_file, delimiter=',')
+        for f in file:
+            print(f) 
 
 def write_to_database(list_vc: list) -> None:
     with open('datebase.csv', 'w+') as csv_file:
@@ -103,38 +81,82 @@ def read_from_datebase():  # ToDo annotation
     return result
 
 
-def show_result_check():
-    result = read_from_datebase()
-    return [int(value[0]) for value in result if value is not None]
-
-
 def download_article_list(msg):
     if '\n' in msg or ' ' in msg:
         list_vendor_code = msg.split()
         if all(value.isdigit() for value in list_vendor_code):
             list_vendor_code = list(map(int, list_vendor_code))
-            list_vendor_code = [[vc, None, True]for vc in list_vendor_code]
+            list_vendor_code = [[vc, None, True] for vc in list_vendor_code]
             write_to_database(list_vendor_code)
             print('Список загружен')
             return True
     return False
 
 
-def start_check_vc():
-    global chat_id
+def send_message():
+    '''Функция рассылки сообщений пользователям'''
+    with open('users_datebase.csv', 'r') as csv_file:
+        file = csv.reader(csv_file, delimiter=',', quotechar='|')
+        users_id = [chat_id[0] for chat_id in file if chat_id[1]]
 
-    list_vendor_code = show_result_check()
-    answer = get_vendor_code(list_vendor_code)
-    write_to_database(answer)
-    for line in answer:
-        if not line[2] or line[2] == 'Error':
-            bot.send_message(chat_id, str(line))
+    data = read_from_datebase()
+    for user in users_id:
+        for line_text in data:
+            if line_text[3] == 'False':
+                bot.send_message(user, line_text[1])
+
+        date_now = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+        errors = sum([1 for i in data if i[2] == 'Error'])
+        bot.send_message(user, f'Время последней проверки: {line_text[-1]}')
+        bot.send_message(user, f'Ошибок {errors}')
+        bot.send_message(user, 'Для просмотра полной информации нажмите "Полная информация последней проверки"')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    with open('users_datebase.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow([message.chat.id, message.from_user.first_name, True])
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Включить уведомления")
+    btn2 = types.KeyboardButton("Выключить уведомления")
+    btn3 = types.KeyboardButton("Просмотр последней проверки")
+    markup.add(btn1, btn2, btn3)
     bot.send_message(
-        chat_id, 'Провека окончена, для просмотра результата ')
+        message.chat.id, text="Привет, {0.first_name}! Я bot версии 0.9 =))"
+        .format(message.from_user), reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if (message.text == "Включить уведомления"):
+        notification_on_off(message.chat.id, True)
+        bot.send_message(
+            message.chat.id, 'Уведомления включены')
+
+    elif (message.text == "Выключить уведомления"):
+        notification_on_off(message.chat.id, False)
+        bot.send_message(
+            message.chat.id, 'Уведомления выключены')
+
+    elif message.text == 'Просмотр последней проверки':
+        answer = read_from_datebase()
+        for line in answer:
+            bot.send_message(message.chat.id, str(line))
+
+    else:
+        if download_article_list(message.text):
+            bot.send_message(message.chat.id, 'Cписок загружен')
+        else:
+            bot.send_message(
+                message.chat.id, 'Разделитель должен быть пробел или ","')
+            bot.send_message(message.chat.id, 'должы быть только цифры')
 
 
 def sheduler():
-    schedule.every(1).minutes.do(start_check_vc)
+    #schedule.every(40).seconds.do(get_vendor_code)
+    #schedule.every(10).seconds.do(send_message)
     while True:
         schedule.run_pending()
         time.sleep(1)
