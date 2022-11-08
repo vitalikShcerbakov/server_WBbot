30a3e22bd3a90c2f2f4692a0a4c3d0d8ffd51b7e
vitalik <sandwich.htc@gmail.com>
2022-11-08 12:30:01 +0300

    Working verson 1.6

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from selenium.webdriver.chrome.service import Service
 from settings import TG_TOKEN
 
 bot = telebot.TeleBot(TG_TOKEN)
-
+TIME_SENDING_MESSAGE = 25
+TIME_CHECK_VENDOR_CODE = 20
 
 def get_vendor_code():
 
@@ -23,37 +24,54 @@ def get_vendor_code():
     options_chrome = webdriver.ChromeOptions()
     options_chrome.add_argument('--headless')
     options_chrome.add_argument('--no-sandbox')
-    #s = Service('/usr/local/bin/chromedriver') 
-    s = Service('/home/ubuntu/dev/WBbot/chromedriver') 
+    s = Service('/home/ubuntu/dev/WBbot/chromedriver')
+    #s = Service('/usr/local/bin/chromedriver')
+
     with webdriver.Chrome(
         options=options_chrome,
          service=s) as browser:
         for i, vc in enumerate(list_vc):
             line = []
             url = f'https://www.wildberries.ru/catalog/{vc}/detail.aspx?targetUrl=BP'
+            line.append(vc)
+            line.append(url)
+            browser.get(url=url)
+            time.sleep(3)
             try:
-                browser.get(url=url)
-                time.sleep(3)
                 lst_value = browser.find_element(
                     By.CLASS_NAME, 'delivery__store')
-                line.append(vc)
-                line.append(url)
                 line.append(lst_value.text)
+
                 if lst_value.text.find('со склада продавца') != -1:
                     line.append(True)
                 else:
                     line.append(False)
-            except Exception:
-                line.append(vc)
-                line.append(url)
-                line.append('Error')
-                line.append('Error')
+
+            except Exception as e:
+                print(f'Not found class "delivery__store": {e}')
+                try:
+                    time.sleep(3)
+                    lst_value = browser.find_element(
+                        By.CLASS_NAME, 'product-line__price-now')
+
+                    if lst_value.text.find('Нет в наличии') == -1:
+                        line.append('Нет в наличии')
+                        line.append(False)
+                    else:
+                        line.append('Неведомая ебанаая хуйня')
+
+                except Exception as e:
+                    print(f'Not found class "product-line__price-now": {e}')
+                    line.append('Error')
+                    line.append('Error')
             finally:
-                progress_bar = i / len(list_vc) * 100
+                date_now = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+                line.append(date_now)
+                progress_bar = (i + 1) / len(list_vc) * 100
                 print(f'Progress: {round(progress_bar, 1)} % {int(progress_bar) * "#"} {i}/{len(list_vc)}')
 
             list_vendor_code.append(line)
-    date_now = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+    
     print(f'Время последнией проверки: {date_now}')
     write_to_database(list_vendor_code)
 
@@ -62,12 +80,10 @@ def write_to_database(list_vc: list) -> None:
     with open('datebase.csv', 'w+') as csv_file:
         writer = csv.writer(csv_file)
         for line in list_vc:
-            date_write = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
-            line.append(date_write)
             writer.writerow(line)
 
 
-def read_from_datebase():  # ToDo annotation
+def read_from_datebase(): 
     result = []
     with open('datebase.csv', 'r') as csv_file:
         spamreader = csv.reader(csv_file, delimiter=',', quotechar='|')
@@ -99,18 +115,23 @@ def send_message():
                 if flag != 'False':
                     users_id.append(id)
             except Exception as e:
-                break                
-
+                print(f'Error added userd in list - send_message: {e}')
+                                
+    bad_product_detected = False
     data = read_from_datebase()
     for user in users_id:
         for line_text in data:
-            if line_text[3] == 'False':
+            if line_text[3] == 'False' and line_text[2] != 'Нет в наличии':
                 bot.send_message(user, line_text[1])
-
-        errors = sum([1 for i in data if i[2] == 'Error'])
+                bad_product_detected = True
+    if bad_product_detected:
+        bad_product = sum([1 for i in data if i[2] == 'Нет в наличии'])
+        errors = sum([1 for i in data if i[3] == 'Error'])
+        if bad_product:
+            bot.send_message(user, f'Товар закончился: {bad_product}')
+        if errors:
+            bot.send_message(user, f'Ошибок: {errors}')
         bot.send_message(user, f'Время последней проверки: {line_text[-1]}')
-        bot.send_message(user, f'Ошибок {errors}')
-
 
 def notification_on_off(chad_id, flag):
     with open('users_datebase.txt', 'r') as file:
@@ -124,7 +145,7 @@ def notification_on_off(chad_id, flag):
                 else:
                     new_data.append(line)
         except Exception as e:
-         print(e)
+         print(f'Error - notification_on_off : {e}')
     with open('users_datebase.txt', 'w') as file:
         file.writelines(new_data)
 
@@ -146,11 +167,14 @@ def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Включить уведомления")
     btn2 = types.KeyboardButton("Выключить уведомления")
-    btn3 = types.KeyboardButton("Просмотр последней проверки")
-    markup.add(btn1, btn2, btn3)
+    btn3 = types.KeyboardButton("Просмотр не выкупленных товаров")
+    btn4 = types.KeyboardButton("Просмотр товаров 'Нет в наличии'")
+    btn5 = types.KeyboardButton("Полный просмотр")
+    markup.add(btn1, btn2, btn3, btn4, btn5)
     bot.send_message(
-        message.chat.id, text="Привет, {0.first_name}! Введите /start"
+        message.chat.id, text="Привет, {0.first_name}! Теперь ты подписан(а) на рассылку =)"
         .format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, f'Уведомления будут приходить каждые {TIME_SENDING_MESSAGE} мин')
 
 
 @bot.message_handler(content_types=['text'])
@@ -165,10 +189,23 @@ def func(message):
         bot.send_message(
             message.chat.id, 'Уведомления выключены')
 
-    elif message.text == 'Просмотр последней проверки':
+    elif message.text == 'Просмотр не выкупленных товаров':
         answer = read_from_datebase()
         for line in answer:
-            bot.send_message(message.chat.id, f'{line[0]} {line[1]} {line[2]}')
+            if line[3] == 'False' and line[2] != 'Нет в наличии':
+                bot.send_message(message.chat.id, f'{line[1]}')
+
+    elif message.text == "Просмотр товаров 'Нет в наличии'":
+        answer = read_from_datebase()
+        for line in answer:
+            if line[2] == 'Нет в наличии':
+                bot.send_message(message.chat.id, f'{line[1]}')
+        bot.send_message(message.chat.id, f'Время последней проверки: {line[-1]}')
+
+    elif message.text == 'Полный просмотр':
+        answer = read_from_datebase()
+        for line in answer:
+            bot.send_message(message.chat.id, f'{line[0]} {line[1]} {line[4]}')
         bot.send_message(message.chat.id, f'Время последней проверки: {line[-1]}')
 
     else:
@@ -181,8 +218,8 @@ def func(message):
 
 
 def sheduler():
-    schedule.every(7).minutes.do(get_vendor_code)
-    schedule.every(10).minutes.do(send_message)
+    schedule.every(TIME_CHECK_VENDOR_CODE).minutes.do(get_vendor_code)
+    schedule.every(TIME_SENDING_MESSAGE).minutes.do(send_message)
     while True:
         schedule.run_pending()
         time.sleep(1)
